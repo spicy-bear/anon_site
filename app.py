73567c2fdea456eaecf09d7a497adf44d744d8ba
Ubuntu <coyote@azure-ubuntu-server.vx4x04kmbpaellvhpxmklisnyd.bx.internal.cloudapp.net>
2023-03-27 20:35:58 +0000

    updates, look, files, new color themes

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import os
-import random
-import string
+import os, random, string
 from flask import Flask, request, redirect, render_template, url_for, send_from_directory
 from redis import Redis
 from werkzeug.utils import secure_filename
+from urllib.parse import urlsplit, urlunsplit, urljoin, urlparse
+from datetime import datetime
 
 app = Flask(__name__)
 redis = Redis()
-
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -17,24 +16,21 @@ if not os.path.exists(UPLOAD_FOLDER):
 def random_string(length):
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 
-#@app.route('/', methods=['GET', 'POST'])
-#def index():
-#    if request.method == 'POST':
-#        url = request.form['url']
-#        if not url:
-#            flash('Please enter a URL')
-#            return redirect(request.url)
-#
-#        short_id = random_string(6)
-#        redis.set(short_id, url)
-#        return render_template('index.html', short_url=f"{request.host_url}{short_id}")
-#
-#    return render_template('index.html')
+def timestamped_filename(filename):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return f"{timestamp}_{filename}"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         url = request.form['url']
+
+        if not url.strip():
+            flash('Please enter a URL')
+            return redirect(request.url)
+
+        if not url.startswith(('http://', 'https://')):
+            url = urljoin('http://', url)
         if not url:
             flash('Please enter a URL')
             return redirect(request.url)
@@ -42,7 +38,7 @@ def index():
         short_id = random_string(6)
         redis.set(short_id, url)
         short_url = f"{request.host_url}{short_id}"
-        return render_template('index.html', short_url=f"{request.host_url}{short_id}")
+        return render_template('index.html', short_url=short_url, original_url=url)
 
     else:
         short_ids = redis.keys()
@@ -57,8 +53,9 @@ def index():
             file_url = url_for('uploaded_file', filename=file_name)
             file_urls.append((file_name, file_url))
 
-        return render_template('index.html', short_urls=short_urls, file_urls=file_urls)
-
+        #return render_template('index.html', short_urls=short_urls, file_urls=file_urls)
+        return render_template('index.html', short_urls=short_urls, file_urls=file_urls, original_url=None)
+        return render_template('index.html', short_url=short_url, original_url=url)
 
 @app.route('/<short_id>')
 def redirect_url(short_id):
@@ -67,6 +64,10 @@ def redirect_url(short_id):
         return redirect(original_url.decode('utf-8'))
     else:
         return "URL not found.", 404
+
+# def timestamped_filename(filename):
+#     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     return f"{timestamp}_{filename}"
 
 @app.route('/file_upload', methods=['GET', 'POST'])
 def file_upload():
@@ -80,13 +81,33 @@ def file_upload():
             return redirect(request.url)
         if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            timestamped_name = timestamped_filename(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], timestamped_name))
+            return redirect(url_for('uploaded_file', filename=timestamped_name))
     return render_template('file_upload.html')
+
+@app.route('/files')
+def file_list():
+    file_names = os.listdir(app.config['UPLOAD_FOLDER'])
+    file_urls = []
+    for file_name in file_names:
+        file_url = url_for('uploaded_file', filename=file_name)
+        file_urls.append((file_name, file_url))
+    return render_template('file_list.html', file_urls=file_urls)
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/links')
+def link_list():
+    short_ids = redis.keys()
+    short_urls = []
+    for short_id in short_ids:
+        short_url = f"{request.host_url}{short_id.decode('utf-8')}"
+        short_urls.append(short_url)
+    return render_template('link_list.html', short_urls=short_urls, redis=redis)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleSwitch = document.getElementById("toggleSwitch");
-    toggleSwitch.addEventListener("change", toggleDarkMode);
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-    function toggleDarkMode() {
-        if (toggleSwitch.checked) {
-            document.body.classList.add("dark-mode");
+    // Check the stored dark mode state on page load
+    if (localStorage.getItem("darkMode") === "enabled") {
+        document.documentElement.classList.add("dark-mode");
+    }
+
+    darkModeToggle.addEventListener("click", function () {
+        document.documentElement.classList.toggle("dark-mode");
+
+        // Save the dark mode state to localStorage
+        if (document.documentElement.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
         } else {
-            document.body.classList.remove("dark-mode");
+            localStorage.removeItem("darkMode");
         }
+    });
+
+    document.getElementById("url_form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        const urlInput = document.getElementById("url_input");
+        if (!urlInput.value.trim()) {
+            alert("Please enter a URL");
+        } else {
+            event.target.submit();
+        }
+    });
+
+    document.getElementById('change-theme-btn').addEventListener('click', function () {
+        let darkThemeEnabled = document.body.classList.toggle('dark-theme');
+        localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+    });
+
+    if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
+        document.body.classList.add('dark-theme');
     }
 });
